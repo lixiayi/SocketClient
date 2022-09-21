4d81a72102f3666dcc25f8a6cf5d18af57f079f3
stoicer <lixy@onecloud.cn>
2022-09-21 11:45:06 +0800

    Initial Commit

---
+++ SockC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SockC
+//
+//  Created by stoicer on 2022/9/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SockC/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SockC
+//
+//  Created by stoicer on 2022/9/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
